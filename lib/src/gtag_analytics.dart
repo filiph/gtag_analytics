8968768059d541ad3f42d5cfb10933bac5f8c5dc
Filip Hracek <filip.hracek@gmail.com>
2017-10-08 22:03:28 -0700

    Fix duplicate library name

--- lib/src/gtag_analytics.dart
+++ lib/src/gtag_analytics.dart
@@ -1,6 +1,6 @@
 // Copyright (c) 2017, filiph. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-library gtag_analytics;
+library gtag_analytics.google_analytics;
 
 import 'package:meta/meta.dart';
 
